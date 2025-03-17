1527cc5ef0176966061169fcb114b431a570a550
ernstudy <hey@ernstudy.com>
2025-03-17 12:56:29 -0400

    Create outils folder and add  LogoApp.jsx components

---
+++ src/outils/LogoApp.jsx
@@ -0,0 +1,11 @@
+import { Feed, Newspaper } from "@mui/icons-material";
+import React from "react";
+
+export default function LogoApp() {
+  return (
+    <div className="logo-app">
+      <Feed className="icon" />
+      <h1>ERNews</h1>
+    </div>
+  );
+}
