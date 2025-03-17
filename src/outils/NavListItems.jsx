8b8f968bf4fcbc289f56896f213e6d9ee8c2c306
ernstudy <hey@ernstudy.com>
2025-03-17 12:59:12 -0400

    Create NavListItems.jsx

---
+++ src/outils/NavListItems.jsx
@@ -0,0 +1,59 @@
+import {
+  Home,
+  Policy,
+  Public,
+  School,
+  SportsHandball,
+} from "@mui/icons-material";
+import { Button, IconButton } from "@mui/material";
+import React, { useState } from "react";
+
+export default function NavListItems() {
+  const [activeItem, setActiveItem] = useState(false);
+  const [isID, setIsID] = useState(null);
+
+  const handleActive = (index) => {
+    setActiveItem(!activeItem);
+    setIsID(index);
+  };
+  const listItems = [
+    {
+      text: "General",
+      icon: <Home />,
+    },
+    {
+      text: "Education",
+      icon: <School />,
+    },
+    {
+      text: "Sport",
+      icon: <SportsHandball />,
+    },
+    {
+      text: "World",
+      icon: <Public />,
+    },
+  ];
+
+  return (
+    <>
+      {listItems.map((item, index) => (
+        <button
+          className={
+            index == isID ? "nav-list-item active-item" : "nav-list-item"
+          }
+          onClick={() => handleActive(index)}
+        >
+          <span
+            className={
+              index == isID ? "custom-icon active-icon" : "custom-icon"
+            }
+          >
+            {item.icon}
+          </span>
+          {item.text}
+        </button>
+      ))}
+    </>
+  );
+}
