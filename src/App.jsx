3d023dc35011fefadd89554a5f55412ee8a075bc
ernstudy <hey@ernstudy.com>
2025-03-17 12:49:43 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SideBar from "./components/SideBar";
+import HomePage from "./components/HomePage";
+
+export default function App() {
+  return (
+    <div className="container-all">
+      <SideBar />
+      <HomePage />
+    </div>
+  );
+}
