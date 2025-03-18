155e4acf3571fc986fba0cbcf12fdfa6d7bd61c7
ernstudy <hey@ernstudy.com>
2025-03-18 17:30:24 -0400

    create MainCard.jsx

---
+++ src/outils/MainCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function MainCard() {
+  return (
+    <div className="main_card">
+      <span className="tag-name">Featured</span>
+      <div className="main_card-title">
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id iusto
+          tenetur tempore qui molestias cupiditate?
+        </h2>
+      </div>
+    </div>
+  );
+}
