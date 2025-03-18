7c069262a7dbb86f6d70f3b3d59b7e75928411c7
ernstudy <hey@ernstudy.com>
2025-03-18 17:29:15 -0400

    create big card component

---
+++ src/outils/BigCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function BigCard() {
+  return (
+    <div className="big_card">
+      <div className="big_card-image"></div>
+      <div className="big_card-title">
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+          veniam!
+        </h2>
+        <div className="big_card-description">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et libero
+            aspernatur tenetur?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
