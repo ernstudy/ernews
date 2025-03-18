39472e99452cfa83bcfb5f792cd172a829a57546
ernstudy <hey@ernstudy.com>
2025-03-18 17:32:39 -0400

    create VerticalCard component

---
+++ src/outils/VerticalCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function VerticalCard() {
+  return (
+    <div className="vertical_cards">
+      <div className="vertical_cards-image"></div>
+      <div className="vertical_cards-title">
+        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing.</h2>
+      </div>
+    </div>
+  );
+}
