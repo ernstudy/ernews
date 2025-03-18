2adf554e7abcf32949b24e17966f00d608f1a1ab
ernstudy <hey@ernstudy.com>
2025-03-18 17:31:49 -0400

    create SmallCard component

---
+++ src/outils/SmallCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function SmallCard() {
+  return (
+    <div className="small_card">
+      <div className="small_card-image"></div>
+      <div className="small_card-title">
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+          rem.
+        </h2>
+      </div>
+    </div>
+  );
+}
