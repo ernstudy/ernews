c1fcf98986cf0a4d6d558c54e35680f089c3413e
ernstudy <hey@ernstudy.com>
2025-03-19 09:14:57 -0400

    add input user preferences functionallity

--- src/outils/SearchBar.jsx
+++ src/outils/SearchBar.jsx
@@ -1,11 +1,35 @@
 import { Search } from "@mui/icons-material";
-import React from "react";
+import React, { useContext, useState } from "react";
+import { NewsContext } from "../context/NewsContext";
 
 export default function SearchBar() {
+  const { setUserPreferences } = useContext(NewsContext);
+  const [value, setValue] = useState("");
+
+  const handleChange = (text) => {
+    setValue(text);
+  };
+
+  const handleSubmit = () => {
+    setUserPreferences((prevPref) => ({ ...prevPref, query: value }));
+  };
+
   return (
     <div className="search-bar">
-      <button className="search-btn">Search</button>
-      <input type="text" placeholder="Search" />
+      <button className="search-btn" onClick={handleSubmit}>
+        Search
+      </button>
+      <input
+        type="text"
+        placeholder="Search"
+        value={value}
+        onChange={(e) => handleChange(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key == "Enter") {
+            handleSubmit();
+          }
+        }}
+      />
     </div>
   );
 }
