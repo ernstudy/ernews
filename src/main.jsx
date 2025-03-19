225a9a0ad53baaba50889bc9047cdd570de15c39
ernstudy <hey@ernstudy.com>
2025-03-19 14:05:46 -0400

    import News Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { NewsProvider } from "./context/NewsContext.jsx";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -10,6 +11,8 @@ import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <NewsProvider>
+      <App />
+    </NewsProvider>
   </StrictMode>
 );
