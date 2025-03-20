2f531503606566bd5f0369afbab1c19b82d094a9
ernstudy <hey@ernstudy.com>
2025-03-20 14:39:34 -0400

    create foooter component

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CopyRIght from "../outils/CopyRIght";
+
+export default function FooterComponent() {
+  return (
+    <div className="footer">
+      <CopyRIght />
+    </div>
+  );
+}
