1166cd057d069bb23ba051727411470fb3535af4
ernstudy <saingillese@gmail.com>
2026-06-14 12:20:13 -0400

    refactor import statements in main.jsx to remove file extensions

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { NewsProvider } from "./context/NewsContext.jsx";
+import App from "./App";
+import { NewsProvider } from "./context/NewsContext";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
